698f07b82c5f9cf7fec9dda906ef1fc0b21adb4e
syeikhan <52174241+syeikhanritzmy@users.noreply.github.com>
2022-05-01 22:57:22 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,39 @@
+
+
+var cards = document.getElementById("app");
+
+var Typewriter = new Typewriter(cards,{
+    loop:false,
+    delay:85
+})
+
+Typewriter
+  .pauseFor(3000)
+  .typeString('Hai , Gimana kabarnya ?')
+  .pauseFor(1500)
+  .typeString(' semoga baik baik yah dimanapun berada  xixixi:D .. <br>')
+  .pauseFor(1400)
+  .typeString('tak dirasa yah hari demi hari telah dilewati ... ')
+  .pauseFor(1000)
+  .typeString(', tahun ke tahun telah di lalui hingga sampai ')
+  .pauseFor(1000)
+  .typeString('di tahun 1443 Hijriah ini .. ')
+  .pauseFor(1200)
+  .typeString('nah mumpung besok lebaran nih yah ')
+  .pauseFor(1000)
+  .typeString('hehe mungkin kata yang paling tepat adalah meminta maaf :) <br> ')
+  .pauseFor(1200)
+  .typeString('iya minta maaf atas apa  yang pernah dilalui , ')
+  .pauseFor(1200)
+  .typeString(' seperti  salah kata .. yang membuat  sakit mungkin apapun itu hehe,')
+  .pauseFor(1100)
+  .typeString(' atau sebuah kesalahpahaman terhadap apa yang terjadi :D,')
+  .pauseFor(1100)
+  .typeString(' atau mungkin perilaku yang saya perbuat kurang berkenan.')
+  .pauseFor(1200)
+  .typeString('semoga dengan permintaan maaf ini , kedepannya insya allah menjadi lebih baik lagi ..:D<br><br> ')
+  .pauseFor(1200)
+  .typeString('salam hangat dari  dari pembuat pesan ini :D<br>')
+  .pauseFor(1400)
+  .typeString('Syeikhan ..')
+  .start()
